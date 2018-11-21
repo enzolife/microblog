943b52f751107fd527db197c81f01be7c74d74d1
enzo.kuang <enzolife@foxmail.com>
2018-11-21 15:46:34 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -11,10 +12,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
-
-if __name__ == '__main__':
-    print(app)
-    print(db)
-    print(migrate)
-    print(login)
+from app import routes, models, errors
--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,17 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
+from app.forms import EditProfileForm
 
 import pandas as pd
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -79,3 +88,33 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+@app.route("/tables")
+def tables():
+    # data = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+    data = pd.read_csv("http://www.shopee-redash.com/api/queries/5776/"
+                       "results.csv?api_key=pG2efVdRgBAEnv03TF4GhzEtgRyztvp0YV8v5j8G")
+    # data.set_index(['A'], inplace=True)
+    # data.index.name = None
+    # data.set_index(['Name'], inplace=True)
+    # data.index.name=None
+    # females = data.loc[data.Gender=='f']
+    return render_template('view.html', tables=[data.to_html()])
